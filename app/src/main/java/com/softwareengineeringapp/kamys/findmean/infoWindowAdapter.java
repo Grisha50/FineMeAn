08d9465c3c28b27f4e434d7b7aea30b656d275c6
crmeinen <cmeinen@wisc.edu>
2016-12-09 20:20:52 -0600

    Add files via upload

--- app/src/main/java/com/softwareengineeringapp/kamys/findmean/infoWindowAdapter.java
+++ app/src/main/java/com/softwareengineeringapp/kamys/findmean/infoWindowAdapter.java
@@ -7,24 +7,35 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.plus.model.people.Person;
 
 public class infoWindowAdapter implements InfoWindowAdapter {
     LayoutInflater inflater = null;
     private TextView buildingName;
-    private TextView buildingList ;
+    private TextView bathroom;
+    private TextView handicap;
+    private TextView elevators;
+    private TextView StudyArea;
 
-    public infoWindowAdapter() {
 
+    public infoWindowAdapter(LayoutInflater inflater) {
+        this.inflater = inflater;
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        View v = inflater.inflate(R.layout.building_info_window, null);
+        View v = inflater.inflate(R.layout.building_window, null );
         if (marker != null) {
-            buildingName = (TextView) v.findViewById(R.id.bName);
+            buildingName = (TextView) v.findViewById(R.id.bname);
             buildingName.setText(marker.getTitle());
-            buildingList = (TextView) v.findViewById(R.id.bList);
-            buildingList. setText(marker.getTitle());
+            bathroom = (TextView) v.findViewById(R.id.Bathroom);
+            bathroom.setText("database");
+            handicap = (TextView) v.findViewById(R.id.Ramps);
+            handicap.setText("database");
+            elevators=(TextView) v.findViewById(R.id.Elevators);
+            elevators.setText("database");
+            StudyArea = (TextView) v.findViewById(R.id.studyarea);
+            StudyArea.setText("database");
         }
         return (v);
     }
